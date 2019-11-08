075c00447605aab27a221049e90b0b068e680406
HieuPhan <hieu.phq@gmail.com>
2019-11-08 15:21:35 +0700

    get async pets
    - modify babelrc

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import { render } from 'react-dom'
-import Pet from './Pet'
 import SearchParams from './SearchParams'
 
 // your code is going to go here
 const App = () => {
   return (
-    <div>
-      <div></div>
-      <h1 id="something-important">Adopt me!</h1>
-      <SearchParams />
-    </div>
+    <React.StrictMode>
+      <div>
+        <div></div>
+        <h1 id="something-important">Adopt me!</h1>
+        <SearchParams />
+      </div>
+    </React.StrictMode>
   )
 }
 
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,18 +1,47 @@
-import React, { useState } from 'react'
-import { ANIMALS } from '@frontendmasters/pet'
+import React, { useState, useEffect } from 'react'
+import pet, { ANIMALS } from '@frontendmasters/pet'
 import useDropdown from './useDropdown'
+import Results from './Results'
 
 const SearchParams = () => {
   const [location, setLocation] = useState('Seattle, WA')
-  // const [animal, setAnimal] = useState('dog')
   const [breeds, setBreeds] = useState([])
   const [animal, AnimalDropdown] = useDropdown('Animal', 'dog', ANIMALS)
-  const [breed, BreedDropdown] = useDropdown('Breed', '', breeds)
+  const [breed, BreedDropdown, setBreed] = useDropdown('Breed', '', breeds)
+  const [pets, setPets] = useState([])
+
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal,
+    })
+
+    setPets(animals || [])
+  }
+
+  useEffect(() => {
+    setBreeds([])
+    setBreed('')
+
+    pet.breeds(animal).then(
+      ({ breeds: breedApi }) => {
+        const breedStrings = breedApi.map(breedObj => breedObj.name)
+        setBreeds(breedStrings)
+      },
+      error => console.error(error),
+    )
+  }, [animal, setBreed, setBreeds])
 
   return (
     <div className="search-params">
       <h1>{location}</h1>
-      <form>
+      <form
+        onSubmit={e => {
+          e.preventDefault()
+          requestPets()
+        }}
+      >
         <label htmlFor="location">
           location
           <input
@@ -26,6 +55,7 @@ const SearchParams = () => {
           <button>Submit</button>
         </label>
       </form>
+      <Results pets={pets} />
     </div>
   )
 }
